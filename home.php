f3bab81f31d3c05692b4ed4cf2b33b0a098bd2ac
Mar'i Yustiardin <92997908+Mayq04@users.noreply.github.com>
2022-04-12 19:59:41 +0800

    Mayq04

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/uts-logo.png" width="200px height="200px" /> <br><br><br>
+<font Size="6" face="Helvetica">Sistem Informasi Akademik</font> <br>
+<font Size="6">Universitas Teknologi Sumbawa</font>
+</center>
